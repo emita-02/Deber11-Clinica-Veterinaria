4fda455bab8d71f5249e8331add0b3bba6730e8e
Emily <emily.pazmino02@epn.edu.ec>
2026-01-02 22:28:00 -0500

    feat: creacion de lista simulada de pacientes

---
+++ src/main/java/com/deber11/clinicaVeterinaria/controller/VeterinariaController.java
@@ -0,0 +1,25 @@
+package com.deber11.clinicaVeterinaria.controller;
+
+import com.deber11.clinicaVeterinaria.model.Mascota;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/veterinaria")
+public class VeterinariaController {
+    //Lista simulada de pacientes en la clinica
+    private List<Mascota> listaMascotas = new ArrayList<>();
+
+    public VeterinariaController(){
+        listaMascotas.add(new Mascota(1L, "Luna", "Perro", "12/05/2021", "Andrea Gómez"));
+        listaMascotas.add(new Mascota(2L, "Kitty", "Gato", "03/11/2020", "Alison Mora"));
+        listaMascotas.add(new Mascota(3L, "Mailo", "Perro", "02/11/2012", "Paulethe Pazmiño"));
+        listaMascotas.add(new Mascota(4L, "Kiara", "Tortuga", "18/07/2019", "Diego Herrera"));
+        listaMascotas.add(new Mascota(5L, "Nala", "Hamster", "21/08/2023", "Mariana López"));
+    }
+
+
+}
